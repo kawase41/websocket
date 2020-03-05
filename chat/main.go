6e49b0523ddbf8f8703bd0d90f14491705d13025
Swift-Beginners <dn41.ra20adtdjw@gmail.com>
2020-03-05 18:51:15 +0900

    create chat

---
+++ chat/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if t.templ == nil {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	}
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+
+	// r := newRoom()
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// http.Handle("/room", r)
+
+	// get the room going
+	// go r.run()
+
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
