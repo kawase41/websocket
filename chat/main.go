c535b7412dec83166c071cc962917dd8c3223f0b
Swift-Beginners <dn41.ra20adtdjw@gmail.com>
2020-03-05 20:31:35 +0900

    Annotation added

--- chat/main.go
+++ chat/main.go
@@ -6,20 +6,22 @@ import (
 	"path/filepath"
 	"text/template"
 	"flag"
+	"sync"
 )
 
 // templ represents a single template
 type templateHandler struct {
+	once     sync.Once
 	filename string
 	templ    *template.Template
 }
 
 // ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if t.templ == nil {
+	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	}
-	t.templ.Execute(w, nil)
+	})
+	t.templ.Execute(w, r)
 }
 
 func main() {
