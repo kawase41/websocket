fb4faa8f8cdc03a3ad433b5e8dffdfb0a727fbef
Swift-Beginners <dn41.ra20adtdjw@gmail.com>
2020-03-06 15:29:04 +0900

    Added user authentication function

--- chat/main.go
+++ chat/main.go
@@ -9,6 +9,11 @@ import (
 	"sync"
 	"websocket/trace"
 	"os"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templ represents a single template
@@ -23,18 +28,34 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}  
+	t.templ.Execute(w, data)
 }
 
 var host = flag.String("host", ":8080", "The host of the application.")
 
 func main() {
 	flag.Parse()
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey("98dfbg7iu2nb4uywevihjw4tuiyub34noilk")
+	gomniauth.WithProviders(
+		github.New("3d1e6ba69036e0624b61", "7e8938928d802e7582908a5eadaaaf22d64babf1", "http://localhost:8080/auth/callback/github"),
+		google.New("508837502868-vqb52tdusscthhod4fuetudqfvg06di3.apps.googleusercontent.com", "r8MuiPzCd0DV2qj_H9krJpgu", "http://localhost:8080/auth/callback/google"),
+		facebook.New("537611606322077", "f9f4d77b3d3f4f5775369f5c9f88f65e", "http://localhost:8080/auth/callback/facebook"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// get the room going
