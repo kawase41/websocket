d05f1380e75e9d95ff6f6242888030d9f4e32552
Swift-Beginners <dn41.ra20adtdjw@gmail.com>
2020-03-06 18:14:38 +0900

    Added logout function

--- chat/auth.go
+++ chat/auth.go
@@ -14,7 +14,7 @@ type authHandler struct {
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
 		// Not certified
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
@@ -64,6 +64,7 @@ func loginHandler(w http.ResponseWriter, r *http.Request)  {
 		}
 		authCookieValue := objx.New(map[string]interface{}{
 			"name": user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name: "auth",
