82e8f1b723c6efe9644a9723eade170f7fddd23c
Swift-Beginners <dn41.ra20adtdjw@gmail.com>
2020-03-07 09:09:24 +0900

    avatar jpg file upload

--- chat/main.go
+++ chat/main.go
@@ -50,7 +50,7 @@ func main() {
 		facebook.New("537611606322077", "f9f4d77b3d3f4f5775369f5c9f88f65e", "http://localhost:8080/auth/callback/facebook"),
 	)
 
-	r := newRoom(UseGravatar)
+	r := newRoom(UseFileSystemAvatar)
 	r.tracer = trace.New(os.Stdout)
 	
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
@@ -69,6 +69,9 @@ func main() {
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/uoloader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/",
+			http.FileServer(http.Dir("./avatars"))))
 
 	// get the room going
 	go r.run()
